ae249281620c88098485495a8733497f0a92e898
klich404 <klich4010@gmail.com>
2021-09-18 16:01:11 +0000

    task 4 | complete

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,34 @@
 """Create a estatus route"""
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+
+
+db_tables = {"state": State,
+             "amenity": Amenity,
+             "city": City,
+             "place": Place,
+             "review": Review,
+             "user": User}
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """give the API status"""
     return {'status': 'OK'}
+
+
+@app_views.route("/stats", strict_slashes=False)
+def count_stats():
+    """endpoint that retrieves the number of each objects by type"""
+    new_dict = {}
+    for key, value in db_tables.items():
+        new_dict[key] = storage.count(value)
+    return jsonify(new_dict)
