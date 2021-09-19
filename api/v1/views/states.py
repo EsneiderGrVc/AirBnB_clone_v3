acefc9bb9a1c53555ee855cad332b7c9a8793b4c
klich404 <klich4010@gmail.com>
2021-09-19 20:31:03 +0000

    task 7 | complete

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """handles all default RESTFul API actions"""
 
-from flask import jsonify, make_response, request, abort
+from flask import jsonify, make_response, request, abort, Response
+from werkzeug.wrappers import response
 from api.v1.views import app_views
 from models.state import State
 from models import storage
@@ -37,3 +38,34 @@ def delete_state(state_id):
             storage.save()
             return {}
     abort(404)
+
+
+@app_views.route("/states", methods=['POST'], strict_slashes=False)
+def post_state():
+    """Creates a State"""
+    if not request.get_json():
+        return abort(Response("Not a JSON", 400))
+    elif 'name' not in request.get_json():
+        return abort(Response("Missing name", 400))
+    else:
+        state = State(**request.get_json())
+        storage.new(state)
+        storage.save()
+        return state.to_dict(), 201
+
+
+@app_views.route("/states/<string:state_id>", methods=['PUT'],
+                 strict_slashes=False)
+def put_state(state_id):
+    """Updates a State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    body_request = request.get_json()
+    if not body_request:
+        abort(Response("Not a JSON", 400))
+    for key, value in body_request.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(state, key, value)
+    storage.save()
+    return state.to_dict(), 200
