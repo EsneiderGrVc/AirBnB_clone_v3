efeccf08c7979dfba57a89eea866bbb11884859b
klich404 <klich4010@gmail.com>
2021-09-19 16:38:57 +0000

    task 7 in process

---
+++ api/v1/views/states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""handles all default RESTFul API actions"""
+
+from flask import jsonify, make_response, request
+from werkzeug.exceptions import abort
+from api.v1.views import app_views
+from models.state import State
+from models import storage
+
+
+@app_views.route("/states", strict_slashes=False)
+def return_states():
+    """return all the states"""
+    states = list(storage.all(State).values())
+    states_list = []
+    for state in states:
+        states_list.append(state.to_dict())
+    return jsonify(states_list)
+
+
+@app_views.route("/states/<string:state_id>", strict_slashes=False)
+def state_obj(state_id):
+    """Retrieves a State object"""
+    if state_id is not None:
+        return(storage.get(State, state_id).to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route("/states/<string:state_id>", methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """delete a state object"""
+    delete_obj = storage.get("State", state_id)
+    delete_obj.delete()
+    storage.save()
+    return {}
